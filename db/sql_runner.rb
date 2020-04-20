9d50f781f075230c17f209ac5d8203fe623e3c25
Sam <samhum90@gmail.com>
2020-04-20 15:34:15 +0100

    tidy sql_runner

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,23 +1,26 @@
-require( 'pg' )
-require( 'uri' )
+require('pg')
+require('uri')
 
 class SqlRunner
-
-  def self.run( sql, values = [] )
-    uri = URI.parse(ENV["DATABASE_URL"])
-    begin
-      db = PG.connect({ 
+  def self.run(sql,values = [])
+    if ENV["DATABASE_URL"]
+      uri = URI.parse(ENV["DATABASE_URL"])
+      database_config = {
         host: uri.host,
         dbname: uri.path[1..-1],
         user: uri.user,
         password: uri.password
-       })
+      }
+    else
+      database_config = {host: "localhost", dbname: "gym"}
+    end
+    begin
+      db = PG.connect(database_config)
       db.prepare("query", sql)
-      result = db.exec_prepared( "query", values )
+      result = db.exec_prepared("query", values)
     ensure
       db.close() if db != nil
     end
     return result
   end
-
 end
